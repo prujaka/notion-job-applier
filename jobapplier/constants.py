bbe24e8d17a1e51d3cf3e13840dd96db9aa9f050
Sergey Tkachenko <sergeyprujina@gmail.com>
2025-06-19 21:01:42 +0200

    feat: handle the config file and add constants

--- jobapplier/constants.py
+++ jobapplier/constants.py
@@ -1,8 +1,19 @@
 from configparser import ConfigParser
+from pathlib import Path
 
+CONFIG_NAME = "config.ini"
+CONFIG_PATH = Path(__file__).resolve().parent.parent / CONFIG_NAME
 
 config = ConfigParser()
-config.read('../config.ini')
+config.read(CONFIG_PATH)
 
-API_KEY = config['secrets']['api_key']
+NOTION_API_KEY = config['secrets']['api_key']
 DATABASE_ID = config['databases']['database_id']
+
+API_HEADERS = {
+    "Authorization": f"Bearer {NOTION_API_KEY}",
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28",
+}
+URL_DATABASE = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+
