3af3afcaa62928c0a83ef5ed8f6cab7b6bc5562d
Sergey Tkachenko <sergeyprujina@gmail.com>
2025-06-27 13:19:31 +0200

    refactor: move cv renamming code to cv.py

--- jobapplier/cv.py
+++ jobapplier/cv.py
@@ -1,6 +1,11 @@
+import shutil
 import string
+
+import pandas as pd
 from nltk.tokenize import word_tokenize
 from unidecode import unidecode
+
+from jobapplier.constants import CV_RAW_PATH, CV_RENAMED_PATH, DATA_PATH
 
 
 def get_sep(lang: str):
@@ -24,3 +29,21 @@ def cvfy(text: str, lang: str) -> str:
     sep = get_sep(lang)
     text = sep.join(tokens)
     return f'cv{sep}{text}'
+
+
+def copy_and_rename_cvs():
+    df_listings = pd.read_csv(DATA_PATH.joinpath("listings.csv"))
+    job_titles = df_listings['job_title'].to_list()
+    languages = df_listings['language'].to_list()
+    cv_filenames = [
+        cvfy(title, lang) + ".pdf" for (title, lang) in
+        zip(job_titles, languages)
+    ]
+    cv_paths_raw = sorted(CV_RAW_PATH.glob("*.pdf"))
+    cv_paths_to_rename = [
+        CV_RENAMED_PATH.joinpath(filename) for filename in cv_filenames
+    ]
+    for (cv_raw, cv_to_rename) in zip(cv_paths_raw, cv_paths_to_rename):
+        shutil.copy2(cv_raw, cv_to_rename)
+
+    return None
--- notebooks/sandbox.py
+++ notebooks/sandbox.py
@@ -6,18 +6,4 @@ from jobapplier.cv import cvfy
 import shutil
 from pathlib import Path
 import requests
-import pandas
 
-
-df_listings = pd.read_csv(DATA_PATH.joinpath("listings.csv"))
-job_titles = df_listings['job_title'].to_list()
-languages = df_listings['language'].to_list()
-cv_filenames = [
-    cvfy(title, lang) + ".pdf" for (title, lang) in zip(job_titles, languages)
-]
-cv_paths_raw = sorted(CV_RAW_PATH.glob("*.pdf"))
-cv_paths_to_rename = [
-    CV_RENAMED_PATH.joinpath(filename) for filename in cv_filenames
-]
-for (cv_raw, cv_to_rename) in zip(cv_paths_raw, cv_paths_to_rename):
-    shutil.copy2(cv_raw, cv_to_rename)
