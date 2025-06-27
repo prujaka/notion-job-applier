a4cc21f5bab1213dc9204e03eaa7b6b658570302
Sergey Tkachenko <sergeyprujina@gmail.com>
2025-06-27 13:05:06 +0200

    wip: rename cv pdfs prototype

--- notebooks/sandbox.py
+++ notebooks/sandbox.py
@@ -1,5 +1,23 @@
-from jobapplier.constants import URL_DATABASE, API_HEADERS
-import requests
+import pandas as pd
 
-search_response = requests.post(url=URL_DATABASE, headers=API_HEADERS)
-search_response_dict = search_response.json()
+from jobapplier.constants import (URL_DATABASE, API_HEADERS, CV_RAW_PATH,
+                                  CV_RENAMED_PATH, DATA_PATH)
+from jobapplier.cv import cvfy
+import shutil
+from pathlib import Path
+import requests
+import pandas
+
+
+df_listings = pd.read_csv(DATA_PATH.joinpath("listings.csv"))
+job_titles = df_listings['job_title'].to_list()
+languages = df_listings['language'].to_list()
+cv_filenames = [
+    cvfy(title, lang) + ".pdf" for (title, lang) in zip(job_titles, languages)
+]
+cv_paths_raw = sorted(CV_RAW_PATH.glob("*.pdf"))
+cv_paths_to_rename = [
+    CV_RENAMED_PATH.joinpath(filename) for filename in cv_filenames
+]
+for (cv_raw, cv_to_rename) in zip(cv_paths_raw, cv_paths_to_rename):
+    shutil.copy2(cv_raw, cv_to_rename)
